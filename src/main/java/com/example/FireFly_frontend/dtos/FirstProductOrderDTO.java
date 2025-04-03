11d7e353b1058adc9e078ce73c1470784776a11a
MirkaanStefanov <135588213+MirkaaStefanov@users.noreply.github.com>
2025-04-03 18:02:11 +0300

    add FirstProductOrderDTO

---
+++ src/main/java/com/example/FireFly_frontend/dtos/FirstProductOrderDTO.java
@@ -0,0 +1,16 @@
+package com.example.FireFly_frontend.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FirstProductOrderDTO {
+
+    private Long id;
+    private FirstProductDTO firstProduct;
+    private int quantity;
+    private boolean done;
+}
