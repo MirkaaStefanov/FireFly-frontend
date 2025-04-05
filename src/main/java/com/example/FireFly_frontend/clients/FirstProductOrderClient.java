caf0a7cca3aaa122e91af54d395d3c5a06780ed6
MirkaanStefanov <135588213+MirkaaStefanov@users.noreply.github.com>
2025-04-05 15:13:18 +0300

    Malzeme siparisleri

--- src/main/java/com/example/FireFly_frontend/clients/FirstProductOrderClient.java
+++ src/main/java/com/example/FireFly_frontend/clients/FirstProductOrderClient.java
@@ -4,12 +4,17 @@ import com.example.FireFly_frontend.dtos.FirstProductOrderDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @FeignClient(name = "fire-fly-firstProductOrder", url = "${backend.base-url}/firstProductOrder")
 public interface FirstProductOrderClient {
+
+    @PostMapping("/create")
+    void createFirstProductOrder(@RequestParam Long firstProductId, @RequestParam int requiredQuantity, @RequestHeader("Authorization") String auth);
 
     @GetMapping("/all")
     List<FirstProductOrderDTO> findAllFirstProductOrders(@RequestHeader("Authorization") String auth);
--- src/main/java/com/example/FireFly_frontend/controllers/FirstProductOrderController.java
+++ src/main/java/com/example/FireFly_frontend/controllers/FirstProductOrderController.java
@@ -1,6 +1,9 @@
 package com.example.FireFly_frontend.controllers;
 
+import com.example.FireFly_frontend.clients.FirstProductClient;
 import com.example.FireFly_frontend.clients.FirstProductOrderClient;
+import com.example.FireFly_frontend.dtos.FinalProductDTO;
+import com.example.FireFly_frontend.dtos.FirstProductDTO;
 import com.example.FireFly_frontend.dtos.FirstProductOrderDTO;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -8,7 +11,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -19,6 +24,22 @@ import java.util.List;
 public class FirstProductOrderController {
 
     private final FirstProductOrderClient firstProductOrderClient;
+    private final FirstProductClient firstProductClient;
+
+    @GetMapping("/create")
+    public String createFinalProductOrder(Model model, HttpServletRequest request){
+        String token = (String) request.getSession().getAttribute("sessionToken");
+        List<FirstProductDTO> firstProductList = firstProductClient.findAll(token);
+        model.addAttribute("firstProducts", firstProductList);
+        return "FirstProductOrder/form";
+    }
+
+    @PostMapping("/create")
+    public String submit(@RequestParam Long id, @RequestParam int quantity, HttpServletRequest request){
+        String token = (String) request.getSession().getAttribute("sessionToken");
+        firstProductOrderClient.createFirstProductOrder(id,quantity,token);
+        return "redirect:/firstProductOrder/all";
+    }
 
     @GetMapping("/all")
     public String allFinalProductOrders(Model model, HttpServletRequest request){
